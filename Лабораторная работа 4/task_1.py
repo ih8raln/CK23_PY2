93ba2e63bb690ec8836cc0078c68f8ea641bf81a
ih8raln <106704903+ih8raln@users.noreply.github.com>
2023-11-17 08:39:27 +0300

    Update task_1.py

--- Лабораторная работа 4/task_1.py
+++ Лабораторная работа 4/task_1.py
@@ -1,13 +1,8 @@
-# TODO решите задачу
 import json
 
 def task(file_path) -> float:
-    result = 0
     with open(file_path) as f:
         data = json.load(f)
-        for d in data:
-            result += d['score'] * d['weight']
-    return round(result, 3)
-
+        return round(sum(d['score'] * d['weight'] for d in data), 3)
 
 print(task('input.json'))
