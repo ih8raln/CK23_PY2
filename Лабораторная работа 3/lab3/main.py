a8ffa2ec3f163f01b9aa72a131994bc7b2d8791e
ih8raln <106704903+ih8raln@users.noreply.github.com>
2024-01-29 20:27:07 +0300

    Исправленная ЛР3

--- Лабораторная работа 3/lab3/main.py
+++ Лабораторная работа 3/lab3/main.py
@@ -36,8 +36,8 @@ class PaperBook(Book):
             raise ValueError("Количество страниц должно быть больше нуля.")
         self._pages = value
 
-    def __str__(self):
-        return f"Бумажная книга {self.name}. Автор {self.author}. Количество страниц: {self.pages}."
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
 
 class AudioBook(Book):
@@ -57,5 +57,5 @@ class AudioBook(Book):
             raise ValueError("Продолжительность должна быть больше нуля.")
         self._duration = value
 
-    def __str__(self):
-        return f"Аудиокнига {self.name}. Автор {self.author}. Продолжительность: {self.duration} ч."
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
