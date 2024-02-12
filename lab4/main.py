37c9c5a78afec638e1278554b2825f1509b4887b
ih8raln <106704903+ih8raln@users.noreply.github.com>
2024-02-12 21:04:28 +0300

    Update main.py

--- lab4/main.py
+++ lab4/main.py
@@ -9,6 +9,12 @@ class Transport:
     def stop(self):
         print(f"{self.name} has stopped.")
 
+    def __str__(self):
+        return f"{self.name} ({self.__class__.__name__})"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.speed})"
+
 
 class Car(Transport):
     def __init__(self, name: str, speed: float, num_wheels: int):
@@ -17,6 +23,12 @@ class Car(Transport):
 
     def honk(self):
         print("Beep beep!")
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.num_wheels} wheels"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.speed}, {self.num_wheels})"
 
 
 class Truck(Transport):
@@ -27,24 +39,32 @@ class Truck(Transport):
     def load_cargo(self):
         print(f"{self.name} is loading cargo.")
 
+    def __str__(self):
+        return f"{super().__str__()}, cargo capacity: {self.cargo_capacity} lbs"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.speed}, {self.cargo_capacity})"
+
+
 class Bus(Transport):
     def __init__(self, name: str, speed: float, passengers: int):
         super().__init__(name, speed)
         self.passengers = passengers
 
     def BusStop(self):
-        print(f"{self.name} have {self.passengers} seats.")
+        print(f"{self.name} has {self.passengers} seats.")
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.passengers} passengers"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.speed}, {self.passengers})"
 
 
 my_car = Car("Tesla Model S", 150, 4)
 my_truck = Truck("Ford F-150", 100, 2000)
 my_bus = Bus("Mercedes-Benz", 60, 52)
 
-my_car.move()
-my_car.honk()
-
-my_truck.move()
-my_truck.load_cargo()
-
-my_bus.stop()
-my_bus.BusStop()
+print(my_car)
+print(my_truck)
+print(my_bus)
